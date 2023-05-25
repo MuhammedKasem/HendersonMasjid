b99028307bddb9953f214611c598d675d32e0ded
Muhammed Kasem <muhammedkasem2002@gmail.com>
2023-05-25 15:46:23 -0400

    Added live updates to amounts raised and goals

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -1,71 +1,58 @@
 import "./HeroSection.css"
 import * as React from "react"
 import PrayerTimes from "../PrayerTimes/PrayerTimes"
-import { Link } from "gatsby"
+import {graphql, Link, useStaticQuery} from "gatsby"
 import QuickEvents from "../QuickEvents/QuickEvents";
 import QuranVerse from "../QuranVerse/QuranVerse";
 
-const events = [
-  {
-    id: 1,
-    title: "Friday Prayer",
-    description: "Join us for Friday prayer at the mosque",
-    date: "Friday, March 25th at 1:00pm",
-    link: "/events/friday-prayer"
-  },
-  {
-    id: 2,
-    title: "Community Iftar",
-    description: "Join us for a community iftar during Ramadan",
-    date: "Wednesday, May 11th at 7:30pm",
-    link: "/events/community-iftar"
-  },
-  {
-    id: 3,
-    title: "Community Iftar",
-    description: "Join us for a community iftar during Ramadan",
-    date: "Wednesday, May 11th at 7:30pm",
-    link: "/events/community-iftar"
-  },
-  {
-    id: 4,
-    title: "Community Iftar",
-    description: "Join us for a community iftar during Ramadan",
-    date: "Wednesday, May 11th at 7:30pm",
-    link: "/events/community-iftar"
-  },
-
-
-];
-
 function HeroSection() {
+
+  const data = useStaticQuery(graphql`
+    query {
+      allContentfulFundraiser {
+    nodes {
+      amountRaised
+      project
+      goal
+    }
+  }
+}
+    
+  `);
+
+  const fundraiser = data.allContentfulFundraiser.nodes[0];
+
+  // format the numbers as money
+  const amountRaised = parseFloat(fundraiser.amountRaised).toLocaleString('en-US');
+  const goal = parseFloat(fundraiser.goal).toLocaleString('en-US');
+
   return (
-    <>
-      <div className="hero-container">
-        <div className="main-container">
-          <div className="sub-container">
-        <h1>Donate Today!</h1>
-          <p>Help support your masjid!</p>
-          <p id="raisedTxt">Total Raised: $193,095.94 </p>
-          <p id="goalTxt">Phase #1 Goal: $500,000</p>
-          <div className="hero-btns">
-            <Link to="/donate">
-              <button className="donateBtn">Donate Now</button>
-              </Link>
-          </div>
-          </div>
+      <>
+        <div className="hero-container">
+          <div className="main-container">
+            <div className="sub-container">
+              <h1>Donate Today!</h1>
+              <p>Help support your masjid!</p>
+              <p id="raisedTxt">Total Raised: ${amountRaised} </p>
+              <p id="goalTxt">Phase #1 Goal: ${goal}</p>
+              <div className="hero-btns">
+                <Link to="/donate">
+                  <button className="donateBtn">Donate Now</button>
+                </Link>
+              </div>
+            </div>
 
-          <div className="quoteContainer">
-            <p id="arabicTxt">مَنْ بَنَى لِلَّهِ مَسْجِدًا بَنَى اللَّهُ لَهُ مِثْلَهُ فِي الْجَنَّةِ</p>
-            <p> "Whoever builds a masjid for the sake of Allah Almighty, like a sparrow's nest or even smaller, Allah Almighty will build for him a house in Paradise."</p>
+            <div className="quoteContainer">
+              <p id="arabicTxt">مَنْ بَنَى لِلَّهِ مَسْجِدًا بَنَى اللَّهُ لَهُ مِثْلَهُ فِي الْجَنَّةِ</p>
+              <p> "Whoever builds a masjid for the sake of Allah Almighty, like a sparrow's nest or even smaller, Allah Almighty will build for him a house in Paradise."</p>
+            </div>
+            <PrayerTimes />
+            <QuickEvents/>
+            <QuranVerse/>
           </div>
-          <PrayerTimes />
-          <QuickEvents events={events}/>
-          <QuranVerse/>
         </div>
-      </div>
 
-    </>
+      </>
   )
 }
 
